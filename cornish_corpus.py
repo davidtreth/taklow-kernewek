6f938b10a261942975d5c15b84f18c92f3b7c3d3
David Trethewey <davidtreth@gmail.com>
2016-01-09 11:04:46 +0000

    made cornish_corpus.py more modular

--- cornish_corpus.py
+++ cornish_corpus.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # David Trethewey 13-04-2015
 #
 # A little on using Python NLTK 
@@ -16,41 +17,43 @@ import nltk
 import matplotlib
 import pylab
 
-def corpusKK():
-    # do imports for traditional (and some revived) texts in Kemmyn
-    import read_kernewek_KK_texts
-    kk_texts, names = read_kernewek_KK_texts.getKKtexts()
-    # use NLTK functions to select words
-    kk_texts_words = [kk_texts.words(i) for i in kk_texts.fileids()]
-    # select those words that are alphabetic
-    kk_texts_words_alpha = [[w for w in t if w.isalpha()] for t in kk_texts_words]
-    # make a NLTK text from the alphabetic words in each of the texts
-    # then put into a list of nltk.Text 
-    #kk_texts_Texts = [nltk.Text(i) for i in kk_texts_words]
-    kk_texts_Texts = [nltk.Text(i) for i in kk_texts_words_alpha]
-    # print kk_texts_words_alpha[0][:10]
-    for i in kk_texts_Texts:
-        print "Text: ",i.name,"\n"
-        print "Collocations: ",i.collocations(),"\n"
-        # frequency distribution
-        fdist_len = nltk.FreqDist([len(w) for w in i])
-        # print fdist_len.keys()
-        # this should already be alphabetic
-        text_alpha = [w for w in i if w.isalpha()]
-        print "number of words = ",len(text_alpha),"\n"
-        print "number of different words = ",len(set([w.lower() for w in text_alpha])),"\n"
-        # frequency distribution (alphabetic)
-        fdist_len2 = nltk.FreqDist([len(w) for w in text_alpha])
-        print "Lengths of words in descending order of frequency", [(length,freq) for length,freq in fdist_len2.items()],"\n"
-        # frequency distribution (alphabetic, lowercase)
-        fdist_alpha = nltk.FreqDist([w.lower() for w in text_alpha])
-        vocab = fdist_alpha.keys()
-        print "Top 50 words: ",vocab[:50],"\n"
-        # frequency distribution (alphabetic, lowercase, more than 4 letters)
-        fdist_alpha_fourormore = nltk.FreqDist([w.lower() for w in text_alpha if len(w)>=4]) 
-        vocab4 = fdist_alpha_fourormore.keys()
-        print "Top 50 words of 4 or more letters: ",vocab4[:50],"\n"
+def basicReport(text):
+    print("Text: ",text.name)
+    print("\n")
+    print("Collocations: {c}\n".format(c=text.collocations()))
+    # frequency distribution
+    fdist_len = nltk.FreqDist([len(w) for w in text])
+    # print fdist_len.keys()
+    # this should already be alphabetic
+    text_alpha = [w for w in text if w.isalpha()]
+    print("number of words = {nw}\n".format(nw=len(text_alpha)))
+    print("number of different words = {ndw}\n".format(ndw = len(set([w.lower() for w in text_alpha]))))
+    # frequency distribution (alphabetic)
+    fdist_len2 = nltk.FreqDist([len(w) for w in text_alpha])
+    print("Lengths of words in descending order of frequency {lw}\n".format(lw = [(length,freq) for length,freq in fdist_len2.items()]))
+    # frequency distribution (alphabetic, lowercase)
+    fdist_alpha = nltk.FreqDist([w.lower() for w in text_alpha])
+    vocab = fdist_alpha.keys()
+    print("Top 50 words: {w50}\n".format(w50=vocab[:50]))
+    # frequency distribution (alphabetic, lowercase, more than 4 letters)
+    fdist_alpha_fourormore = nltk.FreqDist([w.lower() for w in text_alpha if len(w)>=4]) 
+    vocab4 = fdist_alpha_fourormore.keys()
+    print("Top 50 words of 4 or more letters: {w50_4}\n".format(w50_4=vocab4[:50]))
 
+def listPercentsN(text,cfd,dictlist):
+    print "\nText: ",text.name,"\n"
+    fdist = cfd[text.name]
+    # dictionary indexed by length of word
+    lenwordsdict = {}
+    for s in fdist:
+        print s," letters : ",str(100*(float(fdist[s])/len(text)))[:5],"% "
+        lenwordsdict[s] = 100*(float(fdist[s])/len(text))
+    dictlist.append(lenwordsdict)
+    print "\n"
+    #cfd.tabulate(cumulative=True)
+
+        
+def nLettersFDist(kk_texts_Texts,names):
     # conditional frequency distribution
     # based on length of words
     cfd = nltk.ConditionalFreqDist(
@@ -61,16 +64,7 @@ def corpusKK():
     nameslist = [n for n in names]
     #print nameslist
     for t in kk_texts_Texts:
-        print "\nText: ",t.name,"\n"
-        fdist = cfd[t.name]
-        # dictionary indexed by length of word
-        lenwordsdict = {}
-        for s in fdist:
-            print s," letters : ",str(100*(float(fdist[s])/len(t)))[:5],"% "
-            lenwordsdict[s] = 100*(float(fdist[s])/len(t))
-        dictlist.append(lenwordsdict)
-        print "\n"
-        #cfd.tabulate(cumulative=True)
+        listPercentsN(t,cfd,dictlist)
     #print dictlist
     for d in range(len(dictlist)):
         keyslist = [0]+[i[0] for i in dictlist[d].items()]
@@ -88,7 +82,10 @@ def corpusKK():
     pylab.legend()
     pylab.xlabel("Word length")
     pylab.ylabel("Cumulative % frequency")
-
+    
+def compareSamples(kk_texts_Texts,names):
+    # the words to compare abundance of across the texts
+    samples = sorted(["a","ha","an","dhe","yn","yw","ow","ev","rag","mes","esa","yth","y"])
     # conditional frequency distribution
     # based on alphabetic words 
     cfd2 = nltk.ConditionalFreqDist(
@@ -105,8 +102,6 @@ def corpusKK():
     #cfd2.tabulate(cumulative=True)
     #cfd.plot(cumulative=True)
 
-    # the words to compare abundance of across the texts
-    samples = sorted(["a","ha","an","dhe","yn","yw","ow","ev","rag","mes","esa","yth","y"])
     pylab.figure()
     colors = "rgbcmkyw"
     freqs_lists = []
@@ -117,6 +112,7 @@ def corpusKK():
         freqs_lists.append(freqs_list)
     
     ind = pylab.arange(len(samples))
+    nameslist = [n for n in names]
     width = 1.0/(len(nameslist)+1)
 
     bar_groups = []
@@ -131,6 +127,28 @@ def corpusKK():
         pylab.legend([b[0] for b in bar_groups],nameslist)
         pylab.xticks(ind+(len(nameslist)/2.0)*width,samples)
         pylab.tight_layout()
+            
+def corpusKK():
+    # do imports for traditional (and some revived) texts in Kemmyn
+    import read_kernewek_KK_texts
+    kk_texts, names = read_kernewek_KK_texts.getKKtexts()
+    # use NLTK functions to select words
+    kk_texts_words = [kk_texts.words(i) for i in kk_texts.fileids()]
+    # select those words that are alphabetic
+    kk_texts_words_alpha = [[w for w in t if w.isalpha()] for t in kk_texts_words]
+    # make a NLTK text from the alphabetic words in each of the texts
+    # then put into a list of nltk.Text 
+    #kk_texts_Texts = [nltk.Text(i) for i in kk_texts_words]
+    kk_texts_Texts = [nltk.Text(i) for i in kk_texts_words_alpha]
+    # print kk_texts_words_alpha[0][:10]
+    # cycle through the texts
+    # do basic report
+    for i in kk_texts_Texts:
+        basicReport(i)
+
+    nLettersFDist(kk_texts_Texts,names)
+    compareSamples(kk_texts_Texts,names)
+    
 
 
 if __name__ == '__main__':     
--- read_kernewek_KK_texts.py
+++ read_kernewek_KK_texts.py
@@ -3,7 +3,7 @@ from nltk.corpus import PlaintextCorpusReader
 
 def getKKtexts(filepattern=r".*kk.*\.txt",corpus_root = "kernewek_corpus/"):
     # select the Kernewek Kemmyn texts with a regular expression
-    kk_texts = PlaintextCorpusReader(corpus_root,filepattern)
+    kk_texts = PlaintextCorpusReader(corpus_root,filepattern,encoding="latin-1")
     # print names of files to the console
     # to make sure we're selecting the right files
     print kk_texts.fileids(),"\n"
