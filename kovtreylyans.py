88dae44becc3d92923c867d03740f7056414135b
David Trethewey <davidtreth@gmail.com>
2016-08-18 23:19:04 +0100

    separate non-stopword and stopword bigrams and trigrams, sort output by numbers of Ngrams found in common for each sentence.

--- kovtreylyans.py
+++ kovtreylyans.py
@@ -3,6 +3,7 @@
 # which is set up with the example sentences at the
 # ends of chapters of Skeul An Yeth 1 by Wella Brown
 import nltk
+from nltk.corpus import stopwords
 import csv, os
 from operator import itemgetter
 import sys, imp
@@ -27,7 +28,7 @@ def getCorpusSents(sentencefile):
             en_sents[int(row['SentenceNumber'])] = row['English']
     return kw_sents, en_sents
 
-def getCorpusSentBigrams(sentencefile, trigrams = False, casesensit = True, cornish=False):
+def getCorpusSentBigrams(sentencefile, trigrams = False, casesensit = True, cornish=False, maxstopwords=99):
     """ expects a text file which is a comma separated variable file
     with headers including 'SentenceNumber' with an index number
     that should be unique in the corpus, 'Kernewek' with the text
@@ -66,12 +67,22 @@ def getCorpusSentBigrams(sentencefile, trigrams = False, casesensit = True, corn
             # if trigrams is True find trigrams instead
             if trigrams:
                 for t in nltk.trigrams(words):
-                    sents_trigrams.append((row['SentenceNumber'], t))
+                    if cornish:
+                        nstop = 0
+                    else:
+                        nstop=sum([w in stopwords.words('english') for w in t])
+                    if nstop <= maxstopwords:
+                        sents_trigrams.append((row['SentenceNumber'], t))
                 # kwtri = list(nltk.trigrams(kw))
                 # entri = list(nltk.trigrams(en))
             else:
                 for b in nltk.bigrams(words):
-                    sents_bigrams.append((row['SentenceNumber'], b))
+                    if cornish:
+                        nstop = 0
+                    else:
+                        nstop=sum([w in stopwords.words('english') for w in b])
+                    if nstop <= maxstopwords:
+                        sents_bigrams.append((row['SentenceNumber'], b))
                 # kwbi = list(nltk.bigrams(kw))
                 # enbi = list(nltk.bigrams(en))
             #print(kw, en)
@@ -153,25 +164,64 @@ def outputSent(insentwords):
     listinpSb = getBigrams(insentwords)
     print(listinpSb)
     # find common bigrams and trigrams
-    sentNs, bigrs =  findCommonNgrams(skeulyethbi, listinpSb)
-    sentNsT, trigrs =  findCommonNgrams(skeulyethtri, listinpSt)
+    sentNs, bigrs =  findCommonNgrams(skeulyethbi_all, listinpSb)
+    sentNsT, trigrs =  findCommonNgrams(skeulyethtri_all, listinpSt)
+    
+    sentNs_1stop, bigrs_1stop =  findCommonNgrams(skeulyethbi_1stop, listinpSb)
+    sentNsT_2stop, trigrs_2stop =  findCommonNgrams(skeulyethtri_2stop, listinpSt)
     # remove sentence numbers from the list of bigrams
     # if they also appear in the trigrams
     sentNs = [s for s in sentNs if s not in sentNsT]
-    # print the common trigrams, and bigrams
-    # and the sentences where they occur
+    sentNs_1stop = [s for s in sentNs_1stop if s not in sentNsT_2stop]
+    # remove sentence numbers from the list of all bigrams and trigrams
+    # if they also appear in the list of non-stopword bigrams and trigrams
+    sentNsT = [s for s in sentNsT if s not in sentNsT_2stop]
+    sentNs = [s for s in sentNs if s not in sentNs_1stop]
+        
+    # create list of tuples of index number and Ngrams
+    # for N grams containing at least 1 non stopword
+    # i.e. maximum 1 stopword in bigrams, 2 in trigrams.
+    sentNs_big_1stop = [(n, bigrs[n]) for n in sentNs_1stop]
+    sentNs_tri_2stop = [(n, trigrs[n]) for n in sentNsT_2stop]
+    # sort the lists by the first element of the Ngram
+    sentNs_big_1stop = sorted(sentNs_big_1stop, key=itemgetter(1))
+    sentNs_tri_2stop = sorted(sentNs_tri_2stop, key=itemgetter(1))
+    sentNs_big_1stop = sorted(sentNs_big_1stop, key=lambda nlist:len(nlist[1]), reverse=True)
+    sentNs_tri_2stop = sorted(sentNs_tri_2stop, key=lambda nlist:len(nlist[1]), reverse=True)
+    # make a list of the sentence numbers with the same sorting
+    sentNs_1stop = [i[0] for i in sentNs_big_1stop]
+    sentNsT_2stop = [i[0] for i in sentNs_tri_2stop]
+
+    # create list of tuples of index number and Ngrams
+    # for N grams containing all stopwords
     sentNs_big = [(n, bigrs[n]) for n in sentNs]
     sentNs_tri = [(n, trigrs[n]) for n in sentNsT]
+    # sort the lists by the first element of the Ngram
     sentNs_big = sorted(sentNs_big, key=itemgetter(1))
     sentNs_tri = sorted(sentNs_tri, key=itemgetter(1))
+    sentNs_big = sorted(sentNs_big, key=lambda nlist:len(nlist[1]), reverse=True)
+    sentNs_tri = sorted(sentNs_tri, key=lambda nlist:len(nlist[1]), reverse=True)   
+ 
+    # make a list of the sentence numbers with the same sorting
     sentNs = [i[0] for i in sentNs_big]
     sentNsT = [i[0] for i in sentNs_tri]
-    print("\nCommon trigrams:")
+
+    # print the common trigrams, and bigrams
+    # and the sentences where they occur    
+    print("\nListing N grams with a minimum of 1 non-stopword each:")
+    print("Common trigrams:\n")
+    for n in sentNsT_2stop:    
+        print("{kw}  --  {en} {t}".format(kw=kw_sents[n].ljust(60), en=en_sents[n].ljust(60), t = unpacklisttuples(trigrs[n])))
+    print("\nCommon bigrams:\n")
+    for n in sentNs_1stop:    
+        print("{kw}  --  {en} {b}".format(kw=kw_sents[n].ljust(60), en=en_sents[n].ljust(60), b = unpacklisttuples(bigrs[n])))
+    print("\nOther N grams containing only stopwords:")
+    print("Common trigrams:\n")
     for n in sentNsT:    
-        print("{kw}  --  {en} {t}\n".format(kw=kw_sents[n].ljust(60), en=en_sents[n].ljust(60), t = unpacklisttuples(trigrs[n])))
-    print("\nCommon bigrams:")
+        print("{kw}  --  {en} {t}".format(kw=kw_sents[n].ljust(60), en=en_sents[n].ljust(60), t = unpacklisttuples(trigrs[n])))
+    print("\nCommon bigrams:\n")
     for n in sentNs:    
-        print("{kw}  --  {en} {b}\n".format(kw=kw_sents[n].ljust(60), en=en_sents[n].ljust(60), b = unpacklisttuples(bigrs[n])))
+        print("{kw}  --  {en} {b}".format(kw=kw_sents[n].ljust(60), en=en_sents[n].ljust(60), b = unpacklisttuples(bigrs[n])))
 
     
     
@@ -186,8 +236,10 @@ if __name__ == "__main__":
     kw_sents, en_sents = getCorpusSents(skeulanyeth)
     # get corpus bigrams and trigrams as a list of nested tuples
     casesensit = False
-    skeulyethbi = getCorpusSentBigrams(skeulanyeth, casesensit = casesensit)
-    skeulyethtri = getCorpusSentBigrams(skeulanyeth, trigrams=True, casesensit = casesensit)
+    skeulyethbi_1stop = getCorpusSentBigrams(skeulanyeth, casesensit = casesensit, maxstopwords=1)
+    skeulyethtri_2stop = getCorpusSentBigrams(skeulanyeth, trigrams=True, casesensit = casesensit, maxstopwords=2)
+    skeulyethbi_all = getCorpusSentBigrams(skeulanyeth, casesensit = casesensit)
+    skeulyethtri_all = getCorpusSentBigrams(skeulanyeth, trigrams=True, casesensit = casesensit)
     while True:
         # get a sentence from the command-line
         insentwords = getInSentWords(casesensit)
