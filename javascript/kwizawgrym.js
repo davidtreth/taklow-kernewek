b0b79f664713970f804415feedad9227ecdf0277
davidtreth <davidtreth@gmail.com>
2020-10-24 15:53:53 +0100

    styling for Javascript Kwiz Awgrym

--- javascript/kwizawgrym.js
+++ javascript/kwizawgrym.js
@@ -40,15 +40,15 @@ class KwizAwgrym {
     chekkGorthyp(g, kewar) {
      /* check answer against the correct one, returning a bool and a message */
     if (isNaN(g)) {
-        return [false, `Nag yw ${g} niver`];
+        return [false, `<div class='kystmesajerr'>Nag yw ${g} niver</div>`];
     }
     else {
         g = parseInt(g);
         if (g === kewar) {
-            return [true, `Ty a ros ${g}<br>Ewn os`];
+            return [true, `<div class='kystmesajewn'><span>Ty a ros ${g}</span><span>Ewn os</span></div>`];
         }
         else {
-            return [false, `Ty a ros ${g}<br>Kamm os`];
+            return [false, `<div class='kystmesajkamm'><span>Ty a ros ${g}</span><span>Kamm os</span></div>`];
         }
     }
     }    
@@ -143,7 +143,7 @@ rigorthyp() {
     g = document.getElementById("gorthyp").value;
     console.log(g);
     if ((isNaN(g))||g==="") {
-        output = "Res yw gorra niver y'n kyst\n avel bysies, rag ensample '42'";
+        output = "<div class='kystmesajerr'>Res yw gorra niver y'n kyst\n avel bysies, rag ensample '42'</div>";
         niver = -99;
         document.getElementById("kystmesaj").innerHTML = output;        
     }
@@ -165,12 +165,12 @@ rigorthyp() {
          * and up to an extra 1 point for speed */
         this.poyntys += Math.max((10.0-t)/10.0, 0) + 1;
         this.niverewn += 1;
-        tekst = `${this.gorthyptekst}<br>${kewarder[1]}<br>${t.toFixed(1)}s, ${this.niverewn}/${this.nivergwrys}, sommenn poyntys = ${this.poyntys.toFixed(2)}`;
+        tekst = `<div class='kystmesajgorthyptekst'><span>${this.gorthyptekst}</span><span>${this.gorthypbys}</span></div>${kewarder[1]}<div class='kystmesajresults'><span class='kystmesajtime'>${t.toFixed(1)}s</span><span class='kystmesajniver'>${this.niverewn}/${this.nivergwrys}</span><span class='kystmesajpoyntys'>sommenn poyntys = ${this.poyntys.toFixed(2)}</span></div>`;
         fcolour = "dark green";
         }
     else {
         // if the answer was wrong
-    tekst = `${kewarder[1]}<br>${this.gorthyptekst} ${this.gorthypbys}<br>${this.niverewn}/${this.nivergwrys}, sommenn poyntys = ${this.poyntys.toFixed(2)}`;
+    tekst = `<div class='kystmesajgorthyptekst'><span>${this.gorthyptekst}</span><span>${this.gorthypbys}</span></div>${kewarder[1]}<div class='kystmesajresults'><span class='kystmesajniver'>${this.niverewn}/${this.nivergwrys}</span><span class='kystmesajpoyntys'>sommenn poyntys = ${this.poyntys.toFixed(2)}</span></div>`;
     fcolour = "dark red";        
     }
     document.getElementById("kystmesaj").innerHTML = tekst;
@@ -197,15 +197,15 @@ gorfenna() {
     var bonusmsg, endmsg;
     if (this.niverewn === this.Ngovynn) {
         this.bonuspoyntys();
-        bonusmsg = "<br>Keslowena!<br>Ty a worthybis pub govynn yn ewn.<br>Bonus a bymp poynt yw genes!<br>";
+        bonusmsg = "<div class='kystmesajpubewn'><span>Keslowena!</span><span>Ty a worthybis pub govynn yn ewn.</span><p>Bonus a bymp poynt yw genes!</p></div>";
     }
     else if (this.niverewn === 0){
-        bonusmsg = "<br>Truan!<br>Ty a worthybis pub govynn yn kamm!<br>Martesen kath a gerdhas a-dro dha vysowek!<br>";
+        bonusmsg = "<div class='kystmesajpubkamm'><span>Truan!</span><span>Ty a worthybis pub govynn yn kamm!</span><p>Martesen kath a gerdhas a-dro dha vysowek!</p></div>";
     }
     else {
         bonusmsg = "";
     }
-    endmsg = `Ty a wrug ${this.niverewn} kewar a ${this.Ngovynn}. ${bonusmsg}Dha skor yw ${this.poyntys.toFixed(2)} a boyntys`;
+    endmsg = `<div class='kystmesajend'>Ty a wrug ${this.niverewn} kewar a ${this.Ngovynn}. ${bonusmsg}Dha skor yw ${this.poyntys.toFixed(2)} a boyntys</div>`;
     document.getElementById('govynn').innerHTML = "<em>Chanj an dewisow mar mynn'ta ha gwask 'Dalleth' dhe wul an kwiz arta!</em>";    
     document.getElementById("kystmesaj").innerHTML = endmsg;    
     
